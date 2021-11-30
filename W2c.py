65ab60856b84b7e41d397a83311e0d5e7a9d23ec
Alger-Z <zhangjinhua1997@sjtu.edu.cn>
2021-11-29 19:19:19 -0800

    w2c+lstm 2-classify
    1 load all attack trace , use 2000 of normal log and 700+ attack log to train
    2 separte io and visual part from  w2c.py

--- W2c.py
+++ W2c.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 
-import re
+
 import os
 import numpy as np
-import random
-import matplotlib.pyplot as plt
+
+
 # 
 from sklearn.decomposition import IncrementalPCA    # inital reduction
 from sklearn.manifold import TSNE                   # final reduction
@@ -23,7 +23,21 @@ from sklearn.metrics import auc
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers import Embedding
 from keras.layers.recurrent import LSTM
-from keras.models import Sequential
+from keras.models import Sequential, load_model
+
+from load_helper import *
+from visualization import *
+from utils import *
+from io_helper import *
+
+def word2idx(word,word_model):
+  return word_model.wv.vocab[word].index
+
+def idx2word(idx,word_model):
+  return word_model.wv.index2word[idx]
+   
+
+
 
 class MeanEmbeddingVectorizer(object):
     def __init__(self, word2vec):
@@ -33,222 +47,148 @@ class MeanEmbeddingVectorizer(object):
     def fit(self):
         return self
 
-    # 遍历输入词序列中的每个词，取其在此项量表中的向量，若是改词不在词向量词表中（即训练集中未出现），则填0
+    # 遍历输入词序列中的每个词，取其在此向量表中的向量，若是词不在词向量词表中（即训练集中未出现），则填0
     def transform(self, X):
         return np.array([np.mean([self.word2vec[w] for w in words if w in self.word2vec]
                                 or [np.zeros(self.dim)], axis=0)
                         for words in X
                         ])
-
-def load_one_flle(filename):
-    x = []
-    with open(filename) as f:
-        line = f.readline()
-        x = line.strip('\n').split()
-    return x
-
-def load_adfa_training_files(rootdir):
-    x = []
-    y = []
-    list = os.listdir(rootdir)
-    for i in range(0, len(list)):
-        path = os.path.join(rootdir, list[i])
-        if os.path.isfile(path):
-            x.append(load_one_flle(path))
-            y.append(0)
-    return x, y
-
-def dirlist(path, allfile):
-    filelist = os.listdir(path)
-
-    for filename in filelist:
-        filepath = os.path.join(path, filename)
-        if os.path.isdir(filepath):
-            dirlist(filepath, allfile)
-        else:
-            allfile.append(filepath)
-    return allfile
-
-def load_adfa_webshell_files(rootdir):
-    x = []
-    y = []
-    allfile=dirlist(rootdir,[])
-    for file in allfile:
-        if re.match(r"\./ADFA-LD/Attack_Data_Master/Web_Shell_\d+/UAD-W*", file):
-            x.append(load_one_flle(file))
-            y.append(1)
-    return x, y
-
-def word2idx(word,word_model):
-  return word_model.wv.vocab[word].index
-
-def idx2word(idx,word_model):
-  return word_model.wv.index2word[idx]
-
-
-
-def reduce_dimensions(model):
-    num_dimensions = 2  # final num dimensions (2D, 3D, etc)
-
-    vectors = [] # positions in vector space
-    labels = [] # keep track of words to label our data again later
-    for word in model.wv.vocab:
-        vectors.append(model.wv[word])
-        labels.append(word)
-
-    # convert both lists into numpy vectors for reduction
-    vectors = np.asarray(vectors)
-    labels = np.asarray(labels)
-
-    # reduce using t-SNE
-    vectors = np.asarray(vectors)
-    tsne = TSNE(n_components=num_dimensions, random_state=0)
-    vectors = tsne.fit_transform(vectors)
-
-    x_vals = [v[0] for v in vectors]
-    y_vals = [v[1] for v in vectors]
-    return x_vals, y_vals, labels
-
-def roc_plt(y_tar,y_prd):
-    y_prd = [np.argmax(y) for y in y_prd]  # 取出y中元素最大值所对应的索引
-    y_tar = [np.argmax(y) for y in y_tar]
-    fpr, tpr, thresholds_keras = roc_curve(y_tar, y_prd) 
-    auc_ = auc(fpr, tpr)
-    print("AUC : ", auc_)
-    plt.figure()
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.plot(fpr, tpr, label='S3< val (AUC = {:.3f})'.format(auc_))
-    plt.xlabel('False positive rate')
-    plt.ylabel('True positive rate')
-    plt.title('ROC curve')
-    plt.legend(loc='best')
-    plt.savefig('ROC.jpg')
-    #plt.show()    
-    
-def wv_2d(x_vals, y_vals, labels):
-    random.seed(0)
-    plt.figure(figsize=(12, 12))
-    plt.scatter(x_vals, y_vals)
-
-    #
-    # Label randomly subsampled 25 data points
-    #
-    indices = list(range(len(labels)))
-    selected_indices = random.sample(indices, 200)
-    for i in selected_indices:
-        plt.annotate(labels[i], (x_vals[i], y_vals[i]))
-    #plt.show()
-    fig_path =os.getcwd()+'/w2c.png'
-    plt.savefig(fig_path)
-    
-def show_wv(model):
-    print "word emberding vocab: ", model.wv.vocab.keys()
-    # 生成词向量词表，每个词映射到向量
-    words_vocab = dict()
-    for key in model.wv.vocab.keys():
-        nums = map(float, model[key])
-        words_vocab[key] = np.array(nums)
-    #二维展示词向量分布
-    x_vals, y_vals, labels = reduce_dimensions(model)
-    wv_2d(x_vals, y_vals, labels)
-    
-    # 测试sc编号40-70的近义词组
-    # siml=[]
-    # for x in range(40,70,1):
-    #     sc = str(x)
-    #     try:
-    #         idxlist=model.wv.most_similar_cosmul(sc)
-    #         similar,max_value=max(idxlist,key=lambda item:item[1])
-    #         print (sc,similar,'\n')
-    #     except:
-    #         continue
-
-if __name__ == '__main__':
-    x1, y1 = load_adfa_training_files("./ADFA-LD/Training_Data_Master/")  # 训练集（normal）
-    x2, y2 = load_adfa_webshell_files("./ADFA-LD/Attack_Data_Master/")    # 训练集（attack）
-    x3, y3 = load_adfa_training_files("./ADFA-LD/Validation_Data_Master/")  # 验证集（normal）
-
-    # 训练集黑白样本混合
-    x_train = x1 + x2
-    y_train = y1 + y2
-    x_validate = x3 + x2
-    y_validate = y3 + y2
-
-    #w2c训练/加载
-    modelpath = "./word2vec.test.txt"
-    model = None
-    if os.path.isfile(modelpath):
-        # 导入模型
-        print "load modeling..."
-        model = gensim.models.Word2Vec.load(modelpath)
-    else:
-        # 将词嵌入到一个100维的向量空间中
-        model = gensim.models.Word2Vec(x_train, min_count=1, size=50)
-        # 保存模型
-        model.save(modelpath)
-    
-    #提供给LSTM embedding层
-    pretrained_weights = model.wv.syn0
-    vocab_size, emdedding_size = pretrained_weights.shape
-    #查看词向量结果
-    #show_wv(model)
-        
-    
+def build_lstm():
     # LSTM搭建
-    modelpath2="./lstm.test.txt"
+    
     model = Sequential()
     model.add(Embedding(input_dim=vocab_size, output_dim=emdedding_size, 
                         weights=[pretrained_weights]))
-    model.add(LSTM(units=emdedding_size))
-    model.add(Dense(units=vocab_size))
-    model.add(Activation('softmax'))
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
-    
-    history=model.fit(
-                x1, y1,
-                epochs=1,
-                validation_split=0.05)
-    model.summary()
-    model.save(modelpath2)
-    
-    # Make predictions using the validate set
-    y_pre = model.predict(x3)
-    print "Predict result: ", y_pre
-    roc_plt(x3,y_pre)
-    # if 1 in y_pre:
-    #     print y_pre.index(1)
-    #     print "found 1 in pre"
-'''
-    meanVectorizer = MeanEmbeddingVectorizer(words_vocab)
-    # 将训练语料经过词向量表编码成一个行向量（取均值）
-    x_trainVecs = meanVectorizer.transform(x_train)
-    #for i in range(len(x_train)):
-    #    print x_train[i]
-    #    print x_trainVecs[i]
-    #    print ""
-    # 将验证语料经过词向量表编码成一个行向量（取均值）
-    x_validateVecs = meanVectorizer.transform(x_validate)
-    #for i in range(len(x_train)):
-    #    print x_validate[i]
-    #    print x_validateVecs[i]
-    #    print ""
-    
-    # 根据训练集生成KNN模型
-    clf = KNeighborsClassifier(n_neighbors=4).fit(x_trainVecs, y_train)
-    
-    scores = cross_val_score(clf, x_trainVecs, y_train, n_jobs=-1, cv=10)
-    # 反映KNN模型训练拟合的程度
-    print "Training accurate: "
-    print scores
-    print np.mean(scores)
+    model.add(LSTM(units=emdedding_size,return_sequences=True))
+    model.add(LSTM(units=emdedding_size,return_sequences=False))
+    model.add(Dense(2, activation='softmax'))
 
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    
+    return model
+
+
+if __name__ == '__main__':
+    #action={0:"train",1:"predict",2:"debug"}
+    act=0
+    
+    x1, y1 = load_adfa_training_files("./ADFA-LD/Training_Data_Master/")  # 训练集（normal）
+    x2, y2 = load_adfa_webshell_files("./ADFA-LD/Attack_Data_Master/")    # 训练集（attack）
+    x3, y3 = load_adfa_training_files("./ADFA-LD/Validation_Data_Master/")  # 验证集（normal）
+    att_x,att_y =load_adfa_Attack_files("./ADFA-LD/Attack_Data_Master/")
+    # 训练集黑白样本混合
+    x_train_mixed = x1[:2000] + att_x
+    y_train_mixed = y1[:2000] + att_y
+    x_validate_mixed = x3[:2000] + att_x
+    y_validate_mixed = y3[:2000] + att_y
+    x_=x1+x2+x3
+    y_=y1+y2+y3
+    
+    modelpath = "./word2vec.test.txt"
+    modelpath2="./lstm.h5"
+    
+    model = None
+    wv_model=None
+    #w2c训练/加载
+    if os.path.isfile(modelpath):
+        # 导入模型
+        print "load w2c modeling..."
+        wv_model = gensim.models.Word2Vec.load(modelpath)
+    else:
+        # 将词嵌入到一个100维的向量空间中
+        wv_model = gensim.models.Word2Vec(x_, min_count=1, size=50)
+        # 保存模型
+        wv_model.save(modelpath)
+    
+    #提供给LSTM embedding层
+    pretrained_weights = wv_model.wv.syn0
+    vocab_size, emdedding_size = pretrained_weights.shape
+    #查看词向量结果
+    print "word emberding vocab: ", wv_model.wv.vocab.keys()
+    # 生成词向量词表，每个词映射到向量
+    words_vocab = dict()
+    for key in wv_model.wv.vocab.keys():
+        nums = map(float, wv_model[key])
+        words_vocab[key] = np.array(nums)
+    #
+    
+    #show_wv(model.wv.vocab.keys(),wv_model.wv)
+
+    # 尝试用预训练embedding的lstm做文本分类问题如下：
+    # 1 输入没搞懂，现在将样本序列切并填充到指定长度，然后经过词向量编码取均值变成行向量。y是1,0标签。
+    # 看kaggle示例代码作文本分类，输入用的是w2v的词库idx转化，一个词对应一个整数，然后分类也映射到整数
+    # 我的预测输出是对应到词表的向量概率分布。。。不知道怎么回事
+    
+    # lstm 输入填充 
+    from keras.preprocessing import sequence
+    maxlen = 300
+    xtr_pad=sequence.pad_sequences(x_train_mixed,maxlen=maxlen)
+    xte_pad=sequence.pad_sequences(x_validate_mixed,maxlen=maxlen)
+    
+    #正常样本词向量化,将训练语料经过词向量表编码成一个行向量（取均值）
+    meanVectorizer = MeanEmbeddingVectorizer(words_vocab)
+    xtrain_vecs = meanVectorizer.transform(xtr_pad)
+    xtest_vecs=meanVectorizer.transform(xte_pad)
+    
+    foundone(y_validate_mixed,"yval")
+    foundone(y_train_mixed,"ytrain")
+    
+    if os.path.isfile(modelpath2):
+        # 导入模型
+        print "load lstm modeling..."
+        model = load_model(modelpath2)
+    else:
+        model=build_lstm()
+        history=model.fit(
+                    xtrain_vecs, y_train_mixed,
+                    epochs=50,
+                    validation_split=0.05)
+        model.summary()
+        model.save(modelpath2)
+        saveintopickle(history.history,"tr_history.txt")
+    
     # Make predictions using the validate set
-    y_pre = clf.predict(x_validateVecs)
-    print "Predict result: ", y_pre
+    if act == 1 :
+        y_pre = model.predict(xtest_vecs)
+        print "Predict result: ", y_pre
+        
+        #foundone(y_pre,"ypre")
+
+        roc_plt(y_validate_mixed,y_pre)
     # if 1 in y_pre:
     #     print y_pre.index(1)
     #     print "found 1 in pre"
+
+    # meanVectorizer = MeanEmbeddingVectorizer(words_vocab)
+    
+    # x_trainVecs = meanVectorizer.transform(x_train_mixed)
+    # #for i in range(len(x_train)):
+    # #    print x_train[i]
+    # #    print x_trainVecs[i]
+    # #    print ""
+    # # 将验证语料经过词向量表编码成一个行向量（取均值）
+    # x_validateVecs = meanVectorizer.transform(x_validate_mixed)
+    # #for i in range(len(x_train)):
+    # #    print x_validate[i]
+    # #    print x_validateVecs[i]
+    # #    print ""
+    
+    # # 根据训练集生成KNN模型
+    # clf = KNeighborsClassifier(n_neighbors=4).fit(x_trainVecs, y_train_mixed)
+    
+    # scores = cross_val_score(clf, x_trainVecs, y_train_mixed, n_jobs=-1, cv=10)
+    # # 反映KNN模型训练拟合的程度
+    # print "Training accurate: "
+    # print scores
+    # print np.mean(scores)
+
+    # # Make predictions using the validate set
+    # y_pre = clf.predict(x_validateVecs)
+    # print "Predict result: ", y_pre
+    # # if 1 in y_pre:
+    # #     print y_pre.index(1)
+    # #     print "found 1 in pre"
  
-    # 预测的准确度
-    print "Prediction accurate: %2f" % np.mean(y_pre == y_validate)
-'''
+    # # 预测的准确度
+    # print "Prediction accurate: %2f" % np.mean(y_pre == y_validate_mixed)
--- visualization.py
+++ visualization.py
@@ -1,10 +1,93 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import matplotlib.pyplot as plt
+
 import numpy as np
 import pandas 
 import os
-import re
-file="./ADFA-LD/Attack_Data_Master/Web_Shell_2/UAD-WS2-1371.txt"
-if re.match(r"\./ADFA-LD/Attack_Data_Master/Web_Shell_\d+/UAD-W*", file):
-    print "ssssss"
+import random
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+from sklearn.manifold import TSNE
+
+from utils import foundone                   # final reduction
+    
+def wv_2d(x_vals, y_vals, labels):
+    random.seed(0)
+    plt.figure(figsize=(12, 12))
+    plt.scatter(x_vals, y_vals)
+
+    #
+    # Label randomly subsampled 25 data points
+    #
+    indices = list(range(len(labels)))
+    selected_indices = random.sample(indices, 200)
+    for i in selected_indices:
+        plt.annotate(labels[i], (x_vals[i], y_vals[i]))
+    #plt.show()
+    fig_path =os.getcwd()+'/w2c.png'
+    plt.savefig(fig_path)
+    
+
+def reduce_dimensions(words,words_vec):
+    num_dimensions = 2  # final num dimensions (2D, 3D, etc)
+
+    vectors = [] # positions in vector space
+    labels = [] # keep track of words to label our data again later
+    for word in words:
+        vectors.append(words_vec[word])
+        labels.append(word)
+
+    # convert both lists into numpy vectors for reduction
+    vectors = np.asarray(vectors)
+    labels = np.asarray(labels)
+
+    # reduce using t-SNE
+    vectors = np.asarray(vectors)
+    tsne = TSNE(n_components=num_dimensions, random_state=0)
+    vectors = tsne.fit_transform(vectors)
+
+    x_vals = [v[0] for v in vectors]
+    y_vals = [v[1] for v in vectors]
+    return x_vals, y_vals, labels
+
+def show_wv(words_vocab,words_vec):
+
+    #二维展示词向量分布
+    x_vals, y_vals, labels = reduce_dimensions(words_vocab,words_vec)
+    wv_2d(x_vals, y_vals, labels)
+    
+    # 测试sc编号40-70的近义词组
+    # siml=[]
+    # for x in range(40,70,1):
+    #     sc = str(x)
+    #     try:
+    #         idxlist=model.wv.most_similar_cosmul(sc)
+    #         similar,max_value=max(idxlist,key=lambda item:item[1])
+    #         print (sc,similar,'\n')
+    #     except:
+    #         continue
+
+
+
+def roc_plt(y_tr,y_prd):
+    y_prd = [np.argmax(y) for y in y_prd]  # 取出y中元素最大值所对应的索引
+    y_tr = [np.argmax(y) for y in y_tr]
+    y_p=[]
+    for res in y_prd:
+        y_p.append(y_prd.index(np.max(res)))
+    # foundone(y_prd)
+    # foundone(y_tr)
+    
+    fpr, tpr, thresholds_keras = roc_curve(y_tr, y_prd) 
+    auc_ = auc(fpr, tpr)
+    print("AUC : ", auc_)
+    plt.figure()
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.plot(fpr, tpr, label='S3< val (AUC = {:.3f})'.format(auc_))
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.title('ROC curve')
+    plt.legend(loc='best')
+    plt.savefig('ROC.png')
+    #plt.show() 
